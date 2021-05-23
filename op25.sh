09bfffa7cf3f1b64f31aa5ff5837b31ce35b45e4
rlucente <rlucente@redhat.com>
2021-05-23 16:36:18 -0400

    tweak settings and enable auto tune

--- op25.sh
+++ op25.sh
@@ -3,43 +3,43 @@
 HOSTIP=$(ip route get 8.8.8.8 | sed 's/..*src //g' | awk '{print $1; exit}')
 PORT=8080
 
-cp trunk.tsv fcso.tsv $HOME/op25/op25/gr-op25_repeater/apps
+cp op25.liq meta.json trunk.tsv fcso.tsv ~/op25/op25/gr-op25_repeater/apps
 
-cd /home/pi/op25/op25/gr-op25_repeater/apps
+cd ~/op25/op25/gr-op25_repeater/apps
 
 #
 # The options are:
 #
-# --args 'rtl'                  # Use RTL2832U chipset usb dongle
-# -N 'LNA:30'                   # gain setting
-# -S 2500000                    # source sample rate
-# -x 2                          # audio gain
-# -f 854.9875e6                 # USRP center frequency
-# -o 17e3                       # tuning offset frequency [to circumvent DC offset]
-# -q 0                          # frequency correction in steps of 1200 Hz
-# -d 0                          # fine tuning in steps of 100 Hz
-# -T trunk.tsv                  # trunk config file name
-# -V                            # voice coded
-# -2                            # enable phase2 tdma decode
-# -U                            # enable built-in udp audio player
-# -O hw:CARD=Headphones,DEV=0   # 3.5mm audio jack as output
-# -l http:$HOSTIP:$PORT         # http terminal type
+# --nocrypt                # silence encrypted traffic
+# --args "rtl"             # Use RTL2832U chipset usb dongle
+# -gains 'lna:36'          # gain setting
+# -S 960000                # source sample rate
+# -X                       # enable experimental frequency error tracking
+# -q 0                     # frequency correction in steps of 1200 Hz
+# -d 0                     # fine tuning
+# -v 1                     # message debug level
+# -2                       # enable phase2 tdma decode
+# -V                       # voice coded
+# -U                       # enable built-in udp audio player
+# -T trunk.tsv             # trunk config file name
+# -O pulse                 # use pulse audio for output
+# -l http:$HOSTIP:$PORT    # http terminal type
 #
 
 ./rx.py \
-    --args 'rtl' \
-    -N 'LNA:30' \
-    -S 2500000 \
-    -x 2 \
-    -f 854.9875e6 \
-    -o 17e3 \
+    --nocrypt \
+    --args "rtl" \
+    --gains 'lna:36' \
+    -S 960000 \
+    -X \
     -q 0 \
     -d 0 \
-    -T trunk.tsv \
-    -V \
+    -v 1 \
     -2 \
+    -V \
     -U \
-    -O hw:CARD=Headphones,DEV=0 \
-    -l http:$HOSTIP:$PORT
+    -T trunk.tsv \
+    -O pulse \
+    -l http:$HOSTIP:$PORT \
     2> stderr-stream0.2
 
